370c4468892f7b301d9ea02fb2b6acd1f452842c
MunHevin <Hevin@MunHevinui-MacBook-Pro.local>
2018-06-27 09:44:08 +0900

    02-NonRepeatedCharacter

--- 02-nonrepeatedCharacter/nonrepeatedCharacter.js
+++ 02-nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,18 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var arr = string.split('');
+  var obj = {};
+  for (var i = 0; i < arr.length; i++) {
+    if (!obj[arr[i]]) {
+      obj[arr[i]] = 1;
+    } else {
+      obj[arr[i]]++;
+    }
+  }
+  for (var i = 0; i < arr.length; i++) {
+    if (obj[arr[i]] === 1) {
+      return arr[i];
+    }
+  }
 };
