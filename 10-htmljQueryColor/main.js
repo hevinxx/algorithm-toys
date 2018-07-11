3d0e84ed4615fc6248b6a49f3e11f500273a39ea
MunHevin <Hevin@MunHevinui-MacBook-Pro.local>
2018-07-11 09:36:53 +0900

    Toy10 IM07_문해빈 Complete all the steps

--- 10-htmljQueryColor/main.js
+++ 10-htmljQueryColor/main.js
@@ -6,11 +6,23 @@ $(function(){
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
-
-
+  $('p').html(function() {
+    return '<span>' + $(this)[0].innerText + '</span>';
+  });
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
 
   // TODO: your code here!
+  var getRandomColor = function () {
+    var digits = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += digits[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
+  setInterval(function() {
+    return $('span').css('color', getRandomColor());
+  }, 1000);
 });
