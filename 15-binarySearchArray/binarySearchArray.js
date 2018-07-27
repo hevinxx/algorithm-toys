aaaf70e68f029592b908794940423b2ca1a31eb3
MunHevin <Hevin@MunHevinui-MacBook-Pro.local>
2018-07-27 09:23:26 +0900

    toy15 binarySearchArray complete IM07_문해빈

--- 15-binarySearchArray/binarySearchArray.js
+++ 15-binarySearchArray/binarySearchArray.js
@@ -8,6 +8,16 @@
  * console.log(index); // [3]
  */
 
-var binarySearch = function (array, target) {
+var binarySearch = function(array, target) {
+  var lookUp = function(start, end) {
+    var mid = Math.floor((start + end) / 2);
+    if (target < array[mid]) {
+      return lookUp(start, mid);
+    } else if (target > array[mid]) {
+      return lookUp(mid, end);
+    } else {
+      return [mid];
+    }
+  }
+  return lookUp(0, array.length - 1);
 };
-
