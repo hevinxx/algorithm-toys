71a0c40e5820aa31ebebfdd5d1f21582e76e38b8
MunHevin <Hevin@MunHevinui-MacBook-Pro.local>
2018-08-02 09:36:27 +0900

    toy18 실수로 들어감

--- 18-asyncMap/asyncMap.js
+++ 18-asyncMap/asyncMap.js
@@ -40,33 +40,4 @@
 
 
 var asyncMap = function(tasks, callback){
-  var results = [];
-  for (var i = 0; i < tasks.length; i++) {
-    var as = async () => {
-      tasks[i](function(item) {
-         results.push(item);
-      })
-    };
-    as();
-  }
-  callback(results);
 };
-
-console.log(asyncMap([
-   function(cb){
-     setTimeout(function(){
-       cb('one');
-     }, 200);
-   },
-   function(cb){
-     setTimeout(function(){
-       cb('two');
-     }, 100);
-   }
-  ],
-   function(results){
-     // the results array will equal ['one','two'] even though
-     // the second function had a shorter timeout.
-     console.log(results); // ['one', 'two']
-  }));
-
