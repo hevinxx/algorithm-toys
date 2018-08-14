08764011751c97fff11aa5904ddc32b71b110c47
MunHevin <Hevin@MunHevinui-MacBook-Pro.local>
2018-08-14 09:53:41 +0900

    toy27 complete IM07_문해빈

--- 27-queueStack/queueStack.js
+++ 27-queueStack/queueStack.js
@@ -9,16 +9,27 @@
   */
 var Stack = function() {
 
+  this._storage = {};
+  this._size = 0;
+
   // add an item to the top of the stack
-  this.push = function(){
+  this.push = function(item){
+    this._storage[this._size] = item;
+    this._size++;
   };
 
   // remove an item from the top of the stack
   this.pop = function(){
+    if (this._size-- > 0) {
+      var result = this._storage[this._size];
+      delete this._storage[this._size];
+      return result;
+    }
   };
 
   // return the number of items in the stack
   this.size = function(){
+    return this._size;
   };
 };
 
@@ -30,18 +41,32 @@ var Queue = function() {
   var inbox = new Stack();
   var outbox = new Stack();
 
+  this._storage = inbox._storage;
+  this._start = 0;
+  this._end = 0;
+
   // called to add an item to the `queue`
-  this.enqueue = function(){
+  this.enqueue = function(item){
     // TODO: implement `enqueue`
+    inbox.push(item);
+    this._end++;
   };
 
   // called to remove an item from the `queue`
   this.dequeue = function(){
     // TODO: implement `dequeue`
+    if (this._start < this._end) {
+      var result = this._storage[this._start];
+      delete this._storage[this._start];
+      inbox._size--;
+      this._start++;
+      return result;
+    }
   };
 
   // should return the number of items in the queue
   this.size = function(){
     // TODO: implement `size`
+    return inbox.size();
   };
 };
