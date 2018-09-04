a29aac30e4d2f6fd540e6fb643b44a546b6280c2
MunHevin <Hevin@MunHevinui-MacBook-Pro.local>
2018-09-04 09:59:56 +0900

    toy32 complete

--- 32-primeTester/primeTester.js
+++ 32-primeTester/primeTester.js
@@ -4,12 +4,15 @@
  * a prime number, false if it's not.
  */
 
-var primeTester = function(n) {
-  if(typeof n !== 'number' || n < 1 || n % 1 !== 0){
+var primeTester = function (n) {
+  if (typeof n !== 'number' || n <= 1 || n % 1 !== 0) {
     // n isn't a number or n is less than 1 or n is not an integer
     return false;
   }
-  // TODO: return true if n is prime, false otherwise
+  for (var i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) { return false; }
+  }
+  return true;
 };
 
 /* Extra credit: Write a function that generates a list of all prime numbers
@@ -19,6 +22,11 @@ var primeTester = function(n) {
  */
 
 var primeSieve = function (start, end) {
+  result = [];
+  for (var i = start; i <= end; i++) {
+    if (primeTester(i)) result.push(i);
+  }
+  return result;
 };
 
 
